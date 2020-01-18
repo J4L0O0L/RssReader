8def7a3a0240ca9d1bf0b28ad6665bd26a189f7b
MohammadReza Jalilvand <m.jalilvand@mac02.local>
2020-01-18 11:32:01 +0330

    Initial Commit

---
+++ RSSDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RSSDemo
+//
+//  Created by MohammadReza Jalilvand on 1/18/20.
+//  Copyright © 2020 MohammadReza Jalilvand. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
